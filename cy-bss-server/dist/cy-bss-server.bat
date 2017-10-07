d30aa4aa5eb08fe5d938eb03495d918a6de03c0a
antonio <furone.antonio@gmail.com>
2017-10-07 18:36:52 +0200

    07 oct 2017 2

--- cy-bss-server/dist/cy-bss-server.bat
+++ cy-bss-server/dist/cy-bss-server.bat
@@ -1,5 +1,5 @@
 @echo off
-set "CLASSPATH=.\cy-bss-server.jar;.\lib\aopalliance-1.0.jar;.\lib\commons-dbcp-1.4.jar;.\lib\commons-logging-1.2.jar"
+set "CLASSPATH=.\cy-bss-server.jar;.\lib\cy-bss-core.jar;.\lib\aopalliance-1.0.jar;.\lib\commons-dbcp-1.4.jar;.\lib\commons-logging-1.2.jar"
 set "CLASSPATH=%CLASSPATH%;.\lib\commons-pool-1.6.jar;.\lib\logback-classic-1.1.2.jar;.\lib\logback-core-1.1.2.jar;.\lib\mysql-connector-java-5.1.34-bin.jar;.\lib\slf4j-api-1.7.7.jar"
 set "CLASSPATH=%CLASSPATH%;.\lib\spring-aop-4.0.7.RELEASE.jar;.\lib\spring-beans-4.0.7.RELEASE.jar;.\lib\spring-boot-1.1.8.RELEASE.jar;.\lib\spring-boot-autoconfigure-1.1.8.RELEASE.jar"
 set "CLASSPATH=%CLASSPATH%;.\lib\spring-context-4.0.7.RELEASE.jar;.\lib\spring-core-4.0.7.RELEASE.jar;.\lib\spring-expression-4.0.7.RELEASE.jar;.\lib\spring-jdbc-4.0.7.RELEASE.jar"
